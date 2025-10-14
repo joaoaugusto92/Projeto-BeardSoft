f069126d2cc91a9c30f75dba3c12b58434a893f8
joaoaugusto92 <joaosilva9241@gmail.com>
2025-10-14 20:30:34 -0300

    creation of UserEntity and UserRole

---
+++ src/main/java/com/api/beard_soft/domain/user/UserEntity.java
@@ -0,0 +1,33 @@
+package com.api.beard_soft.domain.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class UserEntity {
+
+ private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private String phoneNumber;
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    public UserEntity(@NotBlank(message = "O email é obrigatorio!") @Email(message = "Email inválido!") String email, String password, UserRole Role) {
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+}
---
+++ src/main/java/com/api/beard_soft/domain/user/UserRole.java
@@ -0,0 +1,6 @@
+package com.api.beard_soft.domain.user;
+
+public enum UserRole {
+    ADMIN,
+    CLIENT
+}
