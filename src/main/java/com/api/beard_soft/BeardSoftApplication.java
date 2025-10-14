f6eb92f775140b664ac3ae3db4bc896c76692fce
joaoaugusto92 <joaosilva9241@gmail.com>
2025-10-14 20:21:08 -0300

    creation of the initial project structure

---
+++ src/main/java/com/api/beard_soft/BeardSoftApplication.java
@@ -0,0 +1,13 @@
+package com.api.beard_soft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeardSoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeardSoftApplication.class, args);
+	}
+
+}
